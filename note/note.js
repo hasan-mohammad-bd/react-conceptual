ddc8c7dba44aa0ba47f9a108c6789b38f1120265
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-03-20 08:03:47 +0600

    jsx

---
+++ note/note.js
@@ -0,0 +1,34 @@
+// React js conceptual 
+
+//47-1 Module Introduction Create React App under the hood
+//==========================================================
+
+/* 
+npx: node package execute 
+==============================
+it makes npm works little bit easier.
+it install and execute the programmer at a time.
+
+npm: node package manager
+
+create-react-app: it is a commend line interphase (CLI), it install a default structure of react app.
+
+package.json (file): in future, lots of work have to do in this file. 
+
+index.js: it is the start point of the application.
+
+noscript in public html folder, is a alert, if the javascript is disable.
+
+to see readme.md preview: ctrl + shift + p > markdown open preview.
+
+47-2 What is JSX, React Component, babel transpiler
+//=========================================================
+JSX:
+search in google: jsx in depth.*****
+
+new component must be written in capital letter.
+
+
+*/
+
+ 
