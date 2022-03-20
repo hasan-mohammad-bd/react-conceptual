f5f79c0e8dce4eea02627cc873a36cde2057b30e
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-03-20 15:58:43 +0600

    how react works

--- note/note.js
+++ note/note.js
@@ -41,7 +41,53 @@ there is two types of component:
 #class component 
 #function component 
 
+in react data is {unidirectional}: from parents > child > grandson > grandson's son
+
+Props value can be changed in props element. its only for displaying the data.  
+
+read the article: Components and Props.
+
+47-4 (advanced) How states works, asynchronous, console state
+=============================================================
+
+setSteps(theLastLine) is asynchronized
+
+useState has two element: one is the current value; and other one is the setValue function.
+
+47-5 How React Hook works, send state via props , props vs state
+=============================================================
+react hook: its a special function, it change the UI based on changing the 
+
+useEffect: (side effect)manipulating code out of mine. like browser api.manipulating DOM.
+
+where is state stay and modified. that area is called state full component. 
+
+props is only displaying the data and only readable. this is called **state less component** or **presentational component**.. 
+
+47-6 What is React, when to use it, react vs angular vs vue
+=============================================================
+it is a javaScript library.
+
+****must read the react docs 
+
+thinking in react : must read article.
+
+article or video about: why react is superior to the programming world.
+
+47-7 How react works, Render, virtual dom, diff algorithm, fiber
+============================================================
+
+#virtual DOM : 
+
+
+
+
+
+
+
 
 */
+
+
 
  
